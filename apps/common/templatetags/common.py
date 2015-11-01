6769984f09f1596782f81648dd668d95179dbada
eli <elim.projects@gmail.com>
2015-11-01 22:39:05 +0200

    add commentnts

--- apps/common/templatetags/common.py
+++ apps/common/templatetags/common.py
@@ -8,6 +8,8 @@ register = template.Library()
 
 @register.filter
 def get_scheduler_date_fields(instance):
+    # field that shouldn;t be displayed in template
+    # ex.: exclude_fields = ['field_a', 'field_b', 'field_c']
     exclude_fields = ['id', 'field_a', ]
     instance.fields = [(field.verbose_name, field.value_to_string(instance))
                        for field in instance._meta.fields if field.name not in exclude_fields]
